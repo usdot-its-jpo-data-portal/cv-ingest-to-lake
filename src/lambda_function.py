649ad1dec19de76e2681128bb3ed48ab40b3847e
chuehlien <chuehlien@gmail.com>
2021-08-24 14:13:31 -0400

    Trigger ECS task with same event when lambda is triggered by NYCDOT ingest bucket files

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -17,16 +17,19 @@ from sandbox_exporter.s3 import CvPilotFileMover
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)  # necessary to make sure aws is logging
 
-
 TARGET_BUCKET = os.environ.get('TARGET_BUCKET')
 SOURCE_BUCKET_PREFIX = 'usdot-its-datahub-'
 SOURCE_KEY_PREFIX = os.environ.get('SOURCE_KEY_PREFIX', "")
+ECS_TASK_JSON = json.loads(os.environ.get('ECS_TASK_JSON', '{}'))
 VALIDATION_QUEUE_NAME = os.environ.get('VALIDATION_QUEUE_NAME', None)
 if VALIDATION_QUEUE_NAME:
     VALIDATION_QUEUE_NAME = [i.strip() for i in VALIDATION_QUEUE_NAME.split(',')]
 
 
 def lambda_handler(event, context):
+    run(event)
+
+def run(event, in_lambda=True):
     """AWS Lambda handler. """
 
     mover = CvPilotFileMover(target_bucket=TARGET_BUCKET,
@@ -35,6 +38,15 @@ def lambda_handler(event, context):
                              validation_queue_names=VALIDATION_QUEUE_NAME)
 
     for bucket, key in mover.get_fps_from_event(event):
+        if "nycdot" in bucket and in_lambda is True:
+            ECS_TASK_JSON['overrides']['containerOverrides'] = [
+                {
+                    "name": ECS_TASK_JSON['taskDefinition'], 
+                    "environment": [{"name": "EVENT", "value": json.dumps(event)}]
+                }
+            ]
+            mover.session.client('ecs').run_task(**ECS_TASK_JSON)
+            return
         try:
             mover.move_file(bucket, key)
         except Exception as e:
@@ -47,11 +59,12 @@ def lambda_handler(event, context):
 
 
 if __name__ == '__main__':
+    # Section used by Fargate
     t0 = time()
     event = json.loads(os.environ.get('EVENT'))
     print('Starting event:')
     print(event)
-    lambda_handler(event, None)
+    run(event, in_lambda=False)
     t1 = time()
     print(f'Run completed in {(t1-t0)/60:.02} minutes.')
-    
+    exit()
